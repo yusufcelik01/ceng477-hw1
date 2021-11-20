a4dd7067f5b5f0fdd8987363f1b8464be35b8aaf
brknrsln <1kanarslan@gmail.com>
2021-11-21 02:54:20 +0300

    object intersections are finished

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 all:
-	g++ -O3 parser.cpp ppm.cpp raytracer.cpp raytracer_math.cpp tinyxml2.cpp -o raytracer -std=c++11 -g
+	g++ -O3 parser.cpp ppm.cpp raytracer.cpp raytracer_math.cpp tinyxml2.cpp -o raytracer -std=c++17 -g
 
 test: test.cpp
-	g++ -O3 test.cpp -o test -std=c++11 -g
+	g++ -O3 test.cpp -o test -std=c++17 -g
     
 png:
 	for foo in *.ppm; do convert "$$foo" "$${foo%.ppm}.png"; done
--- raytracer.cpp
+++ raytracer.cpp
@@ -4,19 +4,22 @@
 
 #include <cmath>
 #include "raytracer_math.h"
+#include <limits>
 
 typedef unsigned char RGB[3];
 
-enum IntersecType {none, sphere, triangle, mash};
+enum IntersecType {none, sphere, triangle, mesh};
 
 int RayIntersecObj(const parser::Scene &scene,parser::Ray ray, IntersecType &i_type);
 parser::Intersection intersectRaySphere(const parser::Scene &scene, parser::Ray &eye_ray, int sphere_id);
+parser::Vec3f intersectRayFace(const parser::Scene &scene, parser::Ray &eye_ray, const parser::Face &face);
+float Determinant();
 
 int main(int argc, char* argv[])
 {
     // Sample usage for reading an XML scene file
-    parser::Scene scene;
 
+    parser::Scene scene;
     scene.loadFromXml(argv[1]);
 
     int i,j,k,bar;
@@ -40,7 +43,7 @@ int main(int argc, char* argv[])
         parser::Vec3f q;// top corner coordinate
         parser::Vec3f s;// pixel coordinate
         parser::Vec3f d;// d in the eq r(t)= e+dt
-        float pixel_width, pixel_height;
+        float pixel_width, pixel_height, p_height, p_width;
         
 
         cam = scene.cameras[cam_id];
@@ -57,21 +60,8 @@ int main(int argc, char* argv[])
         v = cam.up;
         u = crossProduct(v, w);
 
-        /*
-        std::cout << "e = " << e.x << "," << e.y << "," << e.z << std::endl
-                  << "w = " << w.x << "," << w.y << "," << w.z << std::endl
-                  << "v = " << v.x << "," << v.y << "," << v.z << std::endl
-                  << "u = " << u.x << "," << u.y << "," << u.z << std::endl;
-
-        std::cout << "\nNear plane things: " << cam.near_plane.x << " "
-                                            << cam.near_plane.y << " "
-                                            << cam.near_plane.z << " "
-                                            << cam.near_plane.w << std::endl;
-
-        */
         pixel_width = (cam.near_plane.y - cam.near_plane.x) / cam.image_width;
         pixel_height = (cam.near_plane.w - cam.near_plane.z) / cam.image_height;
-
         
         temp_vec = vectorScalerMult(cam.near_plane.x, u);//l.u
 
@@ -84,19 +74,16 @@ int main(int argc, char* argv[])
         q = vectorSum(q, temp_vec);
         //q has the position of top left corner
 
-        /*
-        std::cout << "pixel width " << pixel_height << std::endl;
-        std::cout << "pixel height " << pixel_height << std::endl;
-
-        std::cout << "q = " << q.x << "," << q.y << "," << q.z << std::endl;
-        */
-
         i = 0; //pixels' color value
-        float p_height = pixel_height*0.5;
-        float p_width = pixel_width*0.5;
+        p_height = pixel_height*0.5;
+        p_width = pixel_width*0.5;
         for(int y=0; y < cam.image_height; y++){
             for(int x=0; x < cam.image_width; x++){
                 //first compute pixel coordinates;
+                parser::Ray ray;
+                IntersecType i_type = none;//whether it intersects or not
+                parser::Material material;
+                int closest_obj_id = -1;
                 temp_vec = vectorScalerMult(x*pixel_width+p_width, u);
 
                 s = vectorSum(q, temp_vec);//s = q + s_u.u
@@ -108,7 +95,6 @@ int main(int argc, char* argv[])
 
                 d = vectorSum(s, vectorScalerMult(-1.0, e));//s-e
 
-                parser::Ray ray;
                 ray.e = e;
                 ray.d = d;
 
@@ -116,28 +102,38 @@ int main(int argc, char* argv[])
 
                 //calculate spheres' closest
 
-                IntersecType i_type = none;//whether it intersects or not
-                int closest_obj_id = RayIntersecObj(scene,ray,i_type);
+                closest_obj_id = RayIntersecObj(scene,ray,i_type);
 
                 //find the colour of that material
-
-                if(!i_type){
-                    // does not intersect get backround colour
+                switch (i_type){
+                case none:
                     parser::Vec3i bg;//backgroung
                     bg = scene.background_color;
                     image[i++] = bg.x;
                     image[i++] = bg.y;
                     image[i++] = bg.z;
-                }
-                else{
-                    // if there is a sphere just write green
-                    parser::Material material = scene.materials[scene.spheres[closest_obj_id].material_id - 1];
+                    break;
+                case sphere:
+                    material = scene.materials[scene.spheres[closest_obj_id].material_id - 1];
                     image[i++] = 255 * material.diffuse.x;//R
                     image[i++] = 255 * material.diffuse.y;//G
                     image[i++] = 255 * material.diffuse.z;//B
+                    break;
+                case triangle:
+                    material = scene.materials[scene.triangles[closest_obj_id].material_id - 1];
+                    image[i++] = 255 * material.diffuse.x;//R
+                    image[i++] = 255 * material.diffuse.y;//G
+                    image[i++] = 255 * material.diffuse.z;//B
+                    break;
+                case mesh:
+                    material = scene.materials[scene.meshes[closest_obj_id].material_id - 1];
+                    image[i++] = 255 * material.diffuse.x;//R
+                    image[i++] = 255 * material.diffuse.y;//G
+                    image[i++] = 255 * material.diffuse.z;//B
+                    break;
+                // default:
+                //     break;
                 }
-                //write that colour to the array
-
             }
         }
         //print to ppm
@@ -148,33 +144,51 @@ int main(int argc, char* argv[])
     }
     //write_ppm("test.ppm", image, width, height);
 }
-
+// TODO
+// add face id in return along as mesh id
 int RayIntersecObj(const parser::Scene &scene,parser::Ray ray, IntersecType &i_type){
-    float t_min;//closest objects parameter
-    int closest_obj_id;
+    float t_min = __FLT_MAX__;//closest objects parameter
+    int closest_obj_id = -1;
     int numOfSpheres = scene.spheres.size();
-    for(int k = 0; k < numOfSpheres; k++){
-        parser::Intersection intersect = intersectRaySphere(scene,ray, k);
+    int numOfTriangles = scene.triangles.size();
+    int numOfMeshes = scene.meshes.size();
+    for(int i = 0; i < numOfSpheres; i++){
+        parser::Intersection intersect = intersectRaySphere(scene,ray, i);
 
         if(intersect.discriminant >= 0){
             //meaning they intersect
-            if(i_type==none){
+            if(intersect.t1 < t_min){
+                t_min = intersect.t1;
+                closest_obj_id = i;
                 i_type = sphere;
-                //assign the smallest
-                if(intersect.t1 < intersect.t2)
-                    t_min = intersect.t1;
-                else
-                    t_min = intersect.t2;
-                closest_obj_id = k;
             }
-            else{
-                if(intersect.t1 < t_min){
-                    t_min = intersect.t1;
-                    closest_obj_id = k;
-                }
-                if(intersect.t2 < t_min);{
-                    t_min = intersect.t2;
-                    closest_obj_id = k;
+            if(intersect.t2 < t_min);{
+                t_min = intersect.t2;
+                closest_obj_id = i;
+                i_type = sphere;
+            }
+        }
+    }
+    for(int i = 0; i<numOfTriangles;i++){
+        parser::Vec3f b_g_t = intersectRayFace(scene,ray,scene.triangles[i].indices);
+        if(b_g_t.z < t_min && b_g_t.x >= 0 && b_g_t.y >= 0){
+            if(b_g_t.y <= 1 && b_g_t.x <= (1 - b_g_t.y)){
+                t_min = b_g_t.z;
+                closest_obj_id = i;
+                i_type = triangle;
+            }
+        }
+    }
+    for (size_t j = 0; j < numOfMeshes; j++){
+        parser::Mesh currmesh = scene.meshes[j];
+        int numOfFaces = currmesh.faces.size();
+        for(int i = 0; i<numOfFaces;i++){
+            parser::Vec3f b_g_t = intersectRayFace(scene,ray,currmesh.faces[i]);
+            if(b_g_t.z < t_min && b_g_t.x >= 0 && b_g_t.y >= 0){
+                if(b_g_t.y <= 1 && b_g_t.x <= (1 - b_g_t.y)){
+                    t_min = b_g_t.z;
+                    closest_obj_id = j;
+                    i_type = mesh;
                 }
             }
         }
@@ -225,4 +239,61 @@ parser::Intersection intersectRaySphere(const parser::Scene &scene, parser::Ray 
 
         return intersect;
     }
+}
+
+float Determinant(float matrix[][3]){
+    float det = 0;
+    det += matrix[0][0] * matrix[1][1] * matrix[2][2];
+    det += matrix[0][1] * matrix[1][2] * matrix[2][0];
+    det += matrix[0][2] * matrix[1][0] * matrix[2][1];
+
+    det -= matrix[2][0] * matrix[1][1] * matrix[0][2];
+    det -= matrix[2][1] * matrix[1][2] * matrix[0][0];
+    det -= matrix[2][2] * matrix[1][0] * matrix[0][1];
+    return det;
+}
+
+parser::Vec3f intersectRayFace(const parser::Scene &scene, parser::Ray &eye_ray, const parser::Face &face){
+    parser::Vec3f A,B,C,res;
+    float a,b,c,d,e,f,g,h,i,j,k,l;
+    float ei_hf,gf_di,dh_eg,ak_jb,jc_al,bl_kc;
+    float M = -1;
+    // if do not intersec
+    res.x = -1; 
+    res.y = -1;
+    res.z = __FLT_MAX__;
+
+    A = scene.vertex_data[face.v0_id-1];
+    B = scene.vertex_data[face.v1_id-1];
+    C = scene.vertex_data[face.v2_id-1];
+
+    a = A.x - B.x;
+    b = A.y - B.y;
+    c = A.z - B.z;
+    d = A.x - C.x;
+    e = A.y - C.y;
+    f = A.z - C.z;
+    g = eye_ray.d.x;
+    h = eye_ray.d.y;
+    i = eye_ray.d.z;
+    j = A.x - eye_ray.e.x;
+    k = A.y - eye_ray.e.y;
+    l = A.z - eye_ray.e.z;
+
+    gf_di = g*f - d*i;
+    dh_eg = d*h - e*g;
+    ei_hf = e*i - h*f;
+    jc_al = j*c - a*l;
+    bl_kc = b*l - k*c;
+    ak_jb = a*k - j*b;
+    // TODO
+    // try parallization
+
+    M = a*ei_hf + b*gf_di + c*dh_eg;
+    if(M==0.0)
+        return res;
+    res.x = (j*ei_hf + k*gf_di + l*dh_eg)/M; // beta
+    res.y = (i*ak_jb + h*jc_al + g*bl_kc)/M; // gama
+    res.z = (f*ak_jb + e*jc_al + d*bl_kc)/M; // t
+    return res;
 }
